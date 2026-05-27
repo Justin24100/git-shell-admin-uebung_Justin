75e97b40ed060a921391171f13ea0664f17980ae
Justin24100 <DEINE-GITHUB-EMAIL>
2026-05-27 09:54:34 +0200

    Add system information script

---
+++ scripts/systeminfo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "=============================="
+echo "      Systeminformationen"
+echo "=============================="
+
+echo "Benutzername: $(whoami)"
+echo "Hostname: $(hostname)"
+echo "Datum & Uhrzeit: $(date)"
+echo "Arbeitsverzeichnis: $(pwd)"
